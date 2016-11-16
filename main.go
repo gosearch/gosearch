101e439650b1d3946986230360b1167d17227710
David Castillo <castillobgr@gmail.com>
2016-11-15 21:27:34 -0500

    Add help and port flags

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/gosearch/gosearch/http"
 	"github.com/gosearch/gosearch/service"
 )
 
 func main() {
+	port := flag.Int("p", 9093, "The `port` gosearch will listen on.")
+	help := flag.Bool("h", false, "Print gosearch's help.")
+	flag.Parse()
+
+	if *help {
+		flag.Usage()
+		return
+	}
+
 	server := &http.Server{Index: &service.DefaultIndexService{}}
-	server.Listen(9093)
+	server.Listen(*port)
 }
